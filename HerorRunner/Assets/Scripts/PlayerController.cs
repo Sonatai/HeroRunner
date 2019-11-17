263ee9195f5d41d5f41e14cf86f610d0b09c47d1
David Wernhart <david.wernhart96@gmail.com>
2019-11-17 09:44:03 +0100

    now also with assets

---
+++ HerorRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Animation animation;
+    public CharacterController cController;
+    void Start()
+    {
+        animation.wrapMode = WrapMode.Loop;
+        animation.Play("run");
+    }
+
+    void FixedUpdate()
+    {
+        cController.Move(transform.forward*0.1f);
+    }
+}
